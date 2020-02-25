876cf53caaaf271b0c3362bc4d244774c1bfd73f
Bruno Marques <brunomandrade.br@gmail.com>
2020-02-25 18:42:41 -0300

    Trabalhando com classes e heranças

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-
-class Main {
-    inicio(){
-
+class List {
+    constructor() {
+        this.data = [];
+    }
+    add(data) {
+        this.data.push(data);
+        console.log(data);
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+        this.usuario = 'Bruno Marques';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+    
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario();
