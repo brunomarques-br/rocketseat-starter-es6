8ab22a069ef77a585facca3fe8386d3a669d811c
Bruno Marques <bruno.marques@fornecedores.sicoob.com.br>
2020-03-21 22:59:25 -0300

    Iniciando aplicação simples com ES6+

--- main_async_await.js
+++ main_async_await.js
@@ -1,3 +1,7 @@
+/*
+
+AULA 1 - UTILIZACAO BASICA DO ASYNC AWAIT
+
 const minhaPromisse = () => new Promise((resolve, reject) => {
     setTimeout(() => { resolve('Ok') }, 2000);
 });
@@ -11,3 +15,28 @@ async function executaPromise() {
 }
 
 executaPromise();
+
+*/
+
+/* 
+
+AULA 2 - Utilizando AXIOS
+
+// todas funções do axios, retornam 'promises' 
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        } catch (err) {
+            console.warn('Erro na API');
+        }
+        
+    }
+}
+
+Api.getUserInfo('brunomarques-br');
+
+*/
